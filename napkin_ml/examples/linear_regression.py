45c463bd9ddd71bd47e24f467b0efce3955df005
eriklindernoren <eriklindernoren@gmail.com>
2018-01-25 18:00:56 +0100

    Added plots to examples

--- napkin_ml/examples/linear_regression.py
+++ napkin_ml/examples/linear_regression.py
@@ -21,8 +21,23 @@ def main():
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
+    mse = (np.mean(y_test - y_pred)**2)
     # Print MSE
-    print ('MSE: %.4f' % (np.mean(y_test - y_pred)**2))
+    print ('Mean Squared Error: %.4f' % mse)
+
+    y_pred_line = model.predict(X)
+
+    # Color map
+    cmap = plt.get_cmap('viridis')
+
+    # Plot the results
+    m1 = plt.scatter(366 * X_train, y_train, color=cmap(0.9), s=10)
+    m2 = plt.scatter(366 * X_test, y_test, color=cmap(0.5), s=10)
+    plt.plot(366 * X, y_pred_line, color='black', linewidth=2, label="Prediction")
+    plt.suptitle("Linear Regression")
+    plt.title("MSE: %.2f" % mse, fontsize=10)
+    plt.legend((m1, m2), ("Training data", "Test data"), loc='lower right')
+    plt.show()
 
 if __name__ == "__main__":
     main()
